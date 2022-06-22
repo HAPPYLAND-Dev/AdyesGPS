2019b684554a9ac801176215aa2c1a84f45613aa
xiaozhangup <xiaozhangshiw@163.com>
2022-06-23 07:50:38 +0800

    Update

--- src/main/java/me/xiaozhangup/adyesgps/Main.java
+++ src/main/java/me/xiaozhangup/adyesgps/Main.java
@@ -1,6 +1,8 @@
 package me.xiaozhangup.adyesgps;
 
 import ink.ptms.adyeshach.api.AdyeshachAPI;
+import me.xiaozhangup.adyesgps.tools.IString;
+import me.xiaozhangup.adyesgps.tools.Skull;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -16,6 +18,8 @@ public class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        reloadConfig();
         Bukkit.getPluginManager().registerEvents(new Main() , this);
         Bukkit.getPluginCommand("adyesgps").setExecutor((commandSender, command, s, inside) -> {
             Player p = (Player) commandSender;
@@ -29,11 +33,29 @@ public class Main extends JavaPlugin implements Listener {
             for (int i = 0; i < 9; i++) {
                 inventory.setItem(i, board);
             }
-            for (int i = 9; i < 45; i++) {
+            for (int i = 36; i < 45; i++) {
                 inventory.setItem(i, board);
             }
 
-            getConfig().set();
+            for (int i = 9; i < 36; i++) {
+                if (getConfig().getString(i + ".loc") != null) {
+                    inventory.setItem(i , Skull.getSkull(getConfig().getString(i + ".skull"), IString.addColor(getConfig().getString(i + ".name")) , " " , IString.addColor("&e点击前往")));
+                }
+            }
+
+            p.openInventory(inventory);
+
+            return true;
+        });
+        Bukkit.getPluginCommand("adyesgps").setExecutor((commandSender, command, s, inside) -> {
+            Player p = (Player) commandSender;
+            if (!p.isOp()) return false;
+            getConfig().set(inside[0] + "loc" , p.getLocation());
+            getConfig().set(inside[0] + "skull" , "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzY4ZDM0NTQ3YTZiYjZiNjhjYjcyN2FmMzcxNDk5ZTdjMDQzNjdmOWUwNDE5NzFkMWI1YWUyMWEyZGQwMTIwOSJ9fX0=");
+            getConfig().set(inside[0] + "name" , "Path: " + inside[0]);
+            saveConfig();
+            reloadConfig();
+            p.sendMessage("Done");
             return true;
         });
     }
@@ -43,8 +65,9 @@ public class Main extends JavaPlugin implements Listener {
         if (e.getWhoClicked() instanceof Player player) {
             if (e.getInventory().getHolder() instanceof Holder) {
                 e.setCancelled(true);
-                if (e.getRawSlot() = 1) {
-                    //TODO
+                int i = e.getRawSlot() - 8;
+                if (getConfig().getLocation(i + ".loc") != null) {
+                    player.teleportAsync(getConfig().getLocation(i + ".loc"));
                 }
             }
         }
